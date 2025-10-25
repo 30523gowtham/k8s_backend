5fec74f32c10dddfabbb30bc6fbaa7d0656a51f9
30523gowtham <2300030523@kluniversity.in>
2025-10-25 10:43:08 +0530

    Change API endpoint prefix for PatientController

--- hospital-managment/src/main/java/com/hospital/controller/PatientController.java
+++ hospital-managment/src/main/java/com/hospital/controller/PatientController.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/patient")
+@RequestMapping("/patient")
 @CrossOrigin(origins = "*")
 public class PatientController {
 
