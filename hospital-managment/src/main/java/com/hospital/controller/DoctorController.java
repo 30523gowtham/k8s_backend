680db3a7c2f8b19862778e5475d60cc0303efef3
30523gowtham <2300030523@kluniversity.in>
2025-10-25 10:42:55 +0530

    Update DoctorController.java

--- hospital-managment/src/main/java/com/hospital/controller/DoctorController.java
+++ hospital-managment/src/main/java/com/hospital/controller/DoctorController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/doctor")
+@RequestMapping("/doctor")
 @CrossOrigin(origins = "*")
 public class DoctorController {
 
